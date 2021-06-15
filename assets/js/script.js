f2ea7b64cebd15f3c073ff949e7bff6b2f5ac6f8
Richie Finegan <richiefinegan1990@gmail.com>
2021-06-15 07:55:58 +0100

    Added new age calculating function to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,9 +42,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Age Calculator
 
-function ageCalculator() {
-    var birth = document.getElementById("DOB").value;
-    var dob = new Date(birth)
+function getAge(dateString) {
+    var now = new Date();
+    var today = new Date();
+    var yearNow = now.getFullYear();
+    var monthNow = now.getMonth();
+    var dateNow = now.getDate();
+
+    var dob = new Date(dateString.substring(6, 10), dateString.substring(3, 5), dateString.substring(0, 2) - 1);
+
+    var yearDob = dob.getFullYear();
+    var monthDob = dob.getMonth();
+    var dateDob = dob.getDate();
+    var age = {};
+    var ageString = "";
+    var yearString = "";
+    var monthString = "";
+    var dayString = "";
+
+    yearAge = yearNow - yearDob;
+
+    if (monthNow >= monthDob)
+        var monthAge = monthNow - monthDob + 1;
+    else {
+        yearAge--;
+        var monthAge = 12 + monthNow - monthDob
+    }
+    if (dateNow >= dateDob)
+        var dateAge = dateNow - dateDob;
+    else {
+        monthAge--;
+        var dateAge = 31 + dateNow - dateDob;
+
+        if (monthAge < 0) {
+            monthAge = 11;
+            yearAge--;
+        }
+    }
+
+    age = {
+        years: yearAge,
+        months: monthAge,
+        days: dateAge
+    };
 
     
+
 }
